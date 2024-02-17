4d4f9719f389114fe3ca79123fdc7d8e7f5b7294
Adeel Khan <engr.adeelkhan2002@gmail.com>
2024-02-17 10:20:34 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -60,6 +60,7 @@ class Dev(Configuration):
       'allauth.socialaccount',
       'allauth.socialaccount.providers.google',
       'rest_framework',
+      'rest_framework.authtoken',
   ]
 
   MIDDLEWARE = [
@@ -139,7 +140,13 @@ class Dev(Configuration):
           'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
       },
   ]
-
+  REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+  }
 
   # Internationalization
   # https://docs.djangoproject.com/en/3.2/topics/i18n/
