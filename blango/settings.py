c64b4f9108f8ffcb877644a5072918aaa21d8a5d
Adeel Khan <engr.adeelkhan2002@gmail.com>
2024-03-13 10:35:07 +0000

    Finish django-versatileimagefield

--- blango/settings.py
+++ blango/settings.py
@@ -20,7 +20,8 @@ class Dev(Configuration):
 
   # Build paths inside the project like this: BASE_DIR / 'subdir'.
   BASE_DIR = Path(__file__).resolve().parent.parent
-
+  MEDIA_ROOT = BASE_DIR / "media"  
+  MEDIA_URL = "/media/"  
 
   # Quick-start development settings - unsuitable for production
   # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -63,6 +64,7 @@ class Dev(Configuration):
       'rest_framework.authtoken',
       'drf_yasg',
       'django_filters',
+      'versatileimagefield',
   ]
 
   MIDDLEWARE = [
