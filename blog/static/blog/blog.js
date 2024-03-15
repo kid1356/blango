0d6d07a4e6161e61e0b6ab751f4444aa74c0259a
Adeel Khan <engr.adeelkhan2002@gmail.com>
2024-03-15 11:42:58 +0000

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,10 +1,60 @@
-// alert('Hello, world!')
-const theNumber = 1
-let yourName = 'Ben'
+// // alert('Hello, world!')
+// const theNumber = 1
+// let yourName = 'Ben'
 
-if (theNumber === 1) {
-  let yourName = 'Leo'
-  alert(yourName)
+// if (theNumber === 1) {
+//   let yourName = 'Leo'
+//   alert(yourName)
+// }
+
+// // alert(yourName)
+
+class Greeter {
+  constructor (name) {
+    this.name = name
+  }
+
+  getGreeting () {
+    if (this.name === undefined) {
+      return 'Hello, no name'
+    }
+
+    return 'Hello, ' + this.name
+  }
+
+  showGreeting (greetingMessage) {
+    console.log(greetingMessage)
+  }
+
+  greet () {
+    this.showGreeting(this.getGreeting())
+  }
 }
 
-alert(yourName)
+const g = new Greeter('Patchy')  // Put your name here if you like
+g.greet()
+
+class DelayedGreeter extends Greeter {
+  delay = 2000
+
+  constructor (name, delay) {
+    super(name)
+    if (delay !== undefined) {
+      this.delay = delay
+    }
+  }
+
+  greet () {
+    setTimeout(
+      () => {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    )
+  }
+}
+
+const dg2 = new DelayedGreeter('Patchy 2 Seconds')
+dg2.greet()
+
+const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
+dg1.greet()
