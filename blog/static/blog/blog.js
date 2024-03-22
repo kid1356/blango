16b3b4dc6e72f710c720b1b2447cb93427f69387
Adeel Khan <engr.adeelkhan2002@gmail.com>
2024-03-22 10:07:07 +0000

    Finish JSX

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,124 +1,211 @@
-// // // // alert('Hello, world!')
-// // // const theNumber = 1
-// // // let yourName = 'Ben'
+// // // // // alert('Hello, world!')
+// // // // const theNumber = 1
+// // // // let yourName = 'Ben'
 
-// // // if (theNumber === 1) {
-// // //   let yourName = 'Leo'
-// // //   alert(yourName)
+// // // // if (theNumber === 1) {
+// // // //   let yourName = 'Leo'
+// // // //   alert(yourName)
+// // // // }
+
+// // // // // alert(yourName)
+
+// // // class Greeter {
+// // //   constructor (name) {
+// // //     this.name = name
+// // //   }
+
+// // //   getGreeting () {
+// // //     if (this.name === undefined) {
+// // //       return 'Hello, no name'
+// // //     }
+
+// // //     return 'Hello, ' + this.name
+// // //   }
+
+// // //   showGreeting (greetingMessage) {
+// // //     console.log(greetingMessage)
+// // //   }
+
+// // //   greet () {
+// // //     this.showGreeting(this.getGreeting())
+// // //   }
 // // // }
 
-// // // // alert(yourName)
+// // // const g = new Greeter('Patchy')  // Put your name here if you like
+// // // g.greet()
 
-// // class Greeter {
-// //   constructor (name) {
-// //     this.name = name
-// //   }
+// // // class DelayedGreeter extends Greeter {
+// // //   delay = 2000
 
-// //   getGreeting () {
-// //     if (this.name === undefined) {
-// //       return 'Hello, no name'
-// //     }
+// // //   constructor (name, delay) {
+// // //     super(name)
+// // //     if (delay !== undefined) {
+// // //       this.delay = delay
+// // //     }
+// // //   }
 
-// //     return 'Hello, ' + this.name
-// //   }
+// // //   greet () {
+// // //     setTimeout(
+// // //       () => {
+// // //         this.showGreeting(this.getGreeting())
+// // //       }, this.delay
+// // //     )
+// // //   }
+// // // }
 
-// //   showGreeting (greetingMessage) {
-// //     console.log(greetingMessage)
-// //   }
+// // // const dg2 = new DelayedGreeter('Patchy 2 Seconds')
+// // // dg2.greet()
 
-// //   greet () {
-// //     this.showGreeting(this.getGreeting())
-// //   }
+// // // const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
+// // // dg1.greet()
+
+// // function resolvedCallback(data) {
+// //   console.log('Resolved with data ' +  data)
 // // }
 
-// // const g = new Greeter('Patchy')  // Put your name here if you like
-// // g.greet()
+// // function rejectedCallback(message) {
+// //   console.log('Rejected with message ' + message)
+// // }
 
-// // class DelayedGreeter extends Greeter {
-// //   delay = 2000
-
-// //   constructor (name, delay) {
-// //     super(name)
-// //     if (delay !== undefined) {
-// //       this.delay = delay
+// // const lazyAdd = function (a, b) {
+// //   const doAdd = (resolve, reject) => {
+// //     if (typeof a !== "number" || typeof b !== "number") {
+// //       reject("a and b must both be numbers")
+// //     } else {
+// //       const sum = a + b
+// //       resolve(sum)
 // //     }
 // //   }
 
-// //   greet () {
-// //     setTimeout(
-// //       () => {
-// //         this.showGreeting(this.getGreeting())
-// //       }, this.delay
-// //     )
-// //   }
+// //   return new Promise(doAdd)
 // // }
 
-// // const dg2 = new DelayedGreeter('Patchy 2 Seconds')
-// // dg2.greet()
+// // const p = lazyAdd(3, 4)
+// // p.then(resolvedCallback, rejectedCallback)
 
-// // const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
-// // dg1.greet()
+// // lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
 
-// function resolvedCallback(data) {
-//   console.log('Resolved with data ' +  data)
-// }
-
-// function rejectedCallback(message) {
-//   console.log('Rejected with message ' + message)
-// }
-
-// const lazyAdd = function (a, b) {
-//   const doAdd = (resolve, reject) => {
-//     if (typeof a !== "number" || typeof b !== "number") {
-//       reject("a and b must both be numbers")
-//     } else {
-//       const sum = a + b
-//       resolve(sum)
-//     }
+// class ClickButton extends React.Component {
+//   state = {
+//     wasClicked: false
 //   }
 
-//   return new Promise(doAdd)
+//   handleClick () {
+//     this.setState(
+//       {wasClicked: true}
+//     )
+//   }
+
+//   render () {
+//     let buttonText
+
+//     if (this.state.wasClicked)
+//       buttonText = 'Clicked!'
+//     else
+//       buttonText = 'Click Me'
+
+//     return <button
+//       className="btn btn-primary mt-2"
+//       onClick={
+//         () => {
+//           this.handleClick()
+//         }
+//       }
+//     >
+//       {buttonText}
+//     </button>
+//   }
 // }
 
-// const p = lazyAdd(3, 4)
-// p.then(resolvedCallback, rejectedCallback)
+// const domContainer = document.getElementById('react_root')
+// ReactDOM.render(
+//   React.createElement(ClickButton),
+//   domContainer
+// )
 
-// lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
+class PostRow extends React.Component {
+  render () {
+    const post = this.props.post
 
-class ClickButton extends React.Component {
-  state = {
-    wasClicked: false
+    let thumbnail
+
+    if (post.hero_image.thumbnail) {
+      thumbnail = <img src={post.hero_image.thumbnail}/>
+    } else {
+      thumbnail = '-'
+    }
+
+    return <tr>
+      <td>{post.title}</td>
+      <td>
+        {thumbnail}
+      </td>
+      <td>{post.tags.join(', ')}</td>
+      <td>{post.slug}</td>
+      <td>{post.summary}</td>
+      <td><a href={'/post/' + post.slug + '/'}>View</a></td>
+    </tr>
   }
+}
 
-  handleClick () {
-    this.setState(
-      {wasClicked: true}
-    )
+class PostTable extends React.Component {
+  state = {
+    dataLoaded: true,
+    data: {
+      results: [
+        {
+          id: 15,
+          tags: [
+            'django', 'react'
+          ],
+          'hero_image': {
+            'thumbnail': '/media/__sized__/hero_images/snake-419043_1920-thumbnail-100x100-70.jpg',
+            'full_size': '/media/hero_images/snake-419043_1920.jpg'
+          },
+          title: 'Test Post',
+          slug: 'test-post',
+          summary: 'A test post, created for Django/React.'
+        }
+      ]
+    }
   }
 
   render () {
-    let buttonText
+    let rows
+    if (this.state.dataLoaded) {
+      if (this.state.data.results.length) {
+        rows = this.state.data.results.map(post => <PostRow post={post} key={post.id}/>)
+      } else {
+        rows = <tr>
+          <td colSpan="6">No results found.</td>
+        </tr>
+      }
+    } else {
+      rows = <tr>
+        <td colSpan="6">Loading&hellip;</td>
+      </tr>
+    }
 
-    if (this.state.wasClicked)
-      buttonText = 'Clicked!'
-    else
-      buttonText = 'Click Me'
-
-    return React.createElement(
-      'button',
-      {
-        className: 'btn btn-primary mt-2',
-        onClick: () => {
-          this.handleClick()
-        }
-      },
-      buttonText
-    )
+    return <table className="table table-striped table-bordered mt-2">
+      <thead>
+      <tr>
+        <th>Title</th>
+        <th>Image</th>
+        <th>Tags</th>
+        <th>Slug</th>
+        <th>Summary</th>
+        <th>Link</th>
+      </tr>
+      </thead>
+      <tbody>
+      {rows}
+      </tbody>
+    </table>
   }
 }
 
 const domContainer = document.getElementById('react_root')
 ReactDOM.render(
-  React.createElement(ClickButton),
+  React.createElement(PostTable),
   domContainer
 )
